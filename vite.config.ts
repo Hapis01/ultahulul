456a81ae825dd66a2caedcf39fdea517b3873dec
Hapis01 <hapisbatubara@gmail.com>
2026-09-14 17:13:22 +0700

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+
+  base: '/ultahulul/',
+
   server: {
     host: true,
     port: 5173,
